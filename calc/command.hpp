fce74dc2b4b2d69b7c1fcd36257634f11b098d9e
malishevivan <malishev.ivan@list.ru>
2023-11-24 14:08:10 +0300

    add commands

---
+++ calc/command.hpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+float parseAddition() {
+	float a=0, b=0;
+	cout << "first value: ";
+	cin >> a;
+	cout << "second value: ";
+	cin >> b;
+	return a + b;
+}
+
+float parseSubtraction() {
+	float a=0, b=0;
+	cout << "first value: ";
+	cin >> a;
+	cout << "second value: ";
+	cin >> b;
+	return a - b;
+}
+
+float parseMultiplication() {
+	float a=0, b=0;
+	cout << "Enter the first value: ";
+	cin >> a;
+	cout << "Enter the second value: ";
+	cin >> b;
+	return a * b;
+}
+
+float parseDivision() {
+	float a=0, b=0;
+	cout << "Enter the first value: ";
+	cin >> a;
+	cout << "Enter the second value: ";
+	cin >> b;
+	if(b != 0) {
+		return a / b;
+	}else{
+	return 0;
+	}
+}
+
+float parseExponentiation() {
+	float a=0, r=0;
+	int b=0;
+	cout << "Enter the first value: ";
+	cin >> a;
+	cout << "Enter the second value: ";
+	cin >> b;
+	r=pow(a, b);
+	return r;
+}
+
+float parseSquareRoot() {
+	float a=0, r=0;
+	cout << "Enter the value: ";
+	cin >> a;
+	r=sqrt(a);
+	return r;
+}
+
+float parseSine() {
+	float a=0, r=0;
+	cout << "Enter the value: ";
+	cin >> a;
+	r=sin(a);
+	return r;
+}
+
+float parseCosine() {
+	float a=0, r=0;
+	cout << "Enter the value: ";
+	cin >> a;
+	r=cos(a);
+	return r;
+}
+
+float parseTangent() {
+	float a=0, r=0;
+	cout << "Enter the value: ";
+	cin >> a;
+	r=tan(a);
+	return r;
+}
+
+void help() {
+	cout << "\n\n";
+	cout << " Calculator ------------------------------\n";
+	cout << "\t a\t\t Addition of two number\n";
+	cout << "\t m\t\t Subtraction of two number\n";
+	cout << "\t u\t\t Multiplication of two number\n";
+	cout << "\t d\t\t Division of two number\n";
+	cout << "\t p\t\t Exponentiation of two number\n";
+	cout << "\t r\t\t SquareRoot of a number\n";
+	cout << "\t s\t\t Sine of anumber\n";
+	cout << "\t c\t\t Cosine of anumber\n";
+	cout << "\t t\t\t Tangent of a number\n";
+	cout << "\t h\t\t Help\n";
+	cout << "\t e\t\t Exit\n";
+	cout << "\n\n";
+}
+
+string error() {
+	cout << "Error : Input is anonymous!\n";
+}
--- calc/main.cpp
+++ calc/main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include "command.hpp"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main(int argc, char** argv) {
+	unsigned char type;
+	bool flag=true;
+	while(flag) {
+		cout << "------------------\n(h) For help\n(e) To exit\nEnter Your type: ";
+		cin >> type;
+		switch(type) {
+			case 'a':
+				cout << parseAddition() << endl;
+			break;
+			case 'm':
+				cout << parseSubtraction() << endl;
+			break;
+			case 'u':
+				cout << parseMultiplication() << endl;
+			break;
+			case 'd':
+				cout << parseDivision() << endl;
+			break;
+			case 'p':
+				cout << parseExponentiation() << endl;
+			break;
+			case 'r':
+				cout << parseSquareRoot() << endl;
+			break;
+			case 's':
+				cout << parseSine() << endl;
+			break;
+			case 'c':
+				cout << parseCosine() << endl;
+			break;
+			case 't':
+				cout << parseTangent() << endl;
+			break;
+			case 'h':
+				help();
+			break;
+			case 'e':
+				flag=false;
+			break;
+			default:
+				cout << error() << endl;
+			break;
+		}
+	}
+	return 0;
 }
